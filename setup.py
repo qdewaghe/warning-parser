3f5d59411b8e2cf7d92bb1a29728fad4e871324c
qdewaghe <q.dewaghe@gmail.com>
2020-10-29 15:52:13 +0100

    added missingn py_modules and version bump

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as fh:
 
 setup(
     name="warning-parser",
-    version="0.0.2",
+    version="1.0.0",
     url="https://github.com/qdewaghe/warning-parser",
     author="Quentin Dewaghe",
     author_email="q.dewaghe@gmail.com",
-    description="Parses compilers' outputs",
+    description="Parses compilers' outputs to retrieve warnings",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
@@ -20,6 +20,7 @@ setup(
         "Natural Language :: English"
     ],
     python_requires=">=3.6",
+    py_modules=["warning_parser"],
     package_dir={"": "src"},
     extras_require={
         "dev": [
